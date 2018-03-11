1cd58582650c5be7616334d88796caf2288debc6
Craig Hammond <craigham94@gmail.com>
2018-03-11 00:21:23 -0500

    Program opens file selection menu on startup, prints out contents of file, places name of file in piston window title

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
 extern crate piston_window;
+extern crate nfd;
 
+use nfd::Response;
 use piston_window::{PistonWindow, WindowSettings};
 
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
 fn main() {
-    let mut window: PistonWindow = WindowSettings::new("Unnamed N64 Emulator", [320, 240])
+    let result = nfd::open_file_dialog(None, None).unwrap_or_else(|e| {
+        panic!(e);
+    });
+
+    let chosen_file = match result {
+        Response::Okay(file_path) => file_path,
+        _ => "".to_string()
+    };
+
+    let file = File::open(&chosen_file).unwrap();
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents).unwrap();
+    println!("Contents of file {}\n {}", chosen_file, contents);
+
+    let mut window: PistonWindow = WindowSettings::new(format!("Unnamed N64 Emulator - {}", chosen_file), [320, 240])
         .exit_on_esc(true)
         .build()
         .unwrap();
 
-    while let Some(e) = window.next() {
+    while let Some(_) = window.next() {
         //TODO
-    }
+    };
 }
