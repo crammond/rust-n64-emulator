be23206bf403872c6a17da2780a835aed33bf38c
Craig Hammond <craigham94@gmail.com>
2018-03-12 21:41:36 -0400

    Printing out more header info, made file contents immutable on load

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,21 @@ fn main() {
 
     let file = File::open(&chosen_file).unwrap();
     let mut buf_reader = BufReader::new(file);
-    let mut contents: Vec<u8> = Vec::new();
-    let _ = buf_reader.read_to_end(contents.as_mut());
-    // print the internal game name, info via https://www.romhacking.net/forum/index.php?topic=20415.0
-    println!("Internal name of file {}\n {}", chosen_file, String::from_utf8_lossy(&contents[32..52]));
+    let contents = {
+        let mut mut_contents: Vec<u8> = Vec::new();
+        let _ = buf_reader.read_to_end(mut_contents.as_mut());
+        mut_contents
+    };
+    // info via https://www.romhacking.net/forum/index.php?topic=20415.0
+    println!("Initial PI settings: {:?}", &contents[0x0..0x4]);
+    println!("Clock rate override: {:?}", &contents[0x4..0x8]);
+    println!("Entry point: {:?}", &contents[0x8..0xC]);
+    println!("Checksum: {:?}", &contents[0x10..0x18]);
+    println!("Internal name: {}", String::from_utf8_lossy(&contents[0x20..0x33]));
+    println!("Format: {}", char::from(contents[0x3B]));
+    println!("GameID: {:?}", &contents[0x3C..0x3E]);
+    println!("Country Code: {:?}", &contents[0x3E]);
+    println!("Version: {:?}", &contents[0x3F]);
 
     let mut window: PistonWindow = WindowSettings::new(format!("Unnamed N64 Emulator - {}", chosen_file), [320, 240])
         .exit_on_esc(true)
